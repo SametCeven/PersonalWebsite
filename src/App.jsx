7cb70f4b6e897975a0d9fc7a566eba0ce6cb1bfe
SametCeven <49654402+SametCeven@users.noreply.github.com>
2025-02-21 11:29:33 +0300

    component structure added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
-import './App.css'
+import './App.css';
+import Header from './layout/Header';
+import Footer from './layout/Footer';
+import Navbar from './components/Navbar';
+import Introduction from './components/Introduction';
+import Skills from './components/Skills';
+import Profile from './components/Profile';
+import Projects from './components/Projects';
 
 export default function App() {
 
   return (
     <div>
-      <p className='p-5 text-amber-800'>hello</p>
+      <Header/>
+      <Navbar/>
+      <Introduction/>
+      <Skills/>
+      <Profile/>
+      <Projects/>
+      <Footer/>
     </div>
   )
 }
